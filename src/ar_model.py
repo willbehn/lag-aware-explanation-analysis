ebb8ac3957934646cf941a8e7f3f569daa01fda7
William Henrik Behn <behnwilliam@icloud.com>
2026-09-05 10:38:05 +0200

    feat: add simple auto regrssive model with declared lags

---
+++ src/ar_model.py
@@ -0,0 +1,13 @@
+"""
+Simple auto regressive model that have declared specific lags that are used, to 
+predict the next t-1 time step
+
+x_t = delta + cof*x_(t-1) + alpha
+"""
+
+class SingleFeatureARModel:
+    def __init__(self, used_lags: dict[int, float]):
+        self._used_lags = used_lags 
+
+    def predict(self, series):
+        return sum(w * series[k - 1] for k, w in self._used_lags.items())
